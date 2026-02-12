cb695b340355e35d5192e2d6f2e5806aed67179c
MANU <manoellyberty@gmail.com>
2026-02-12 16:11:45 +0100

    navHome 50%

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div>
+    <nav>
+      <router-link :to="{ name: 'home' }" class="nav">Home</router-link>
+    </nav>
+
+    <main>
+      <router-view/>
+    </main>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+div{
+  background-color: aquamarine;
+
+}
+.nav{
+  text-decoration: none;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <h1>Bienvenue sur Home!!!!</h1>
+</template>
+
+<style scoped></style>
