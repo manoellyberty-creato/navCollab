848218fd0c387f42fc0f6727711018faa59d60d6
Koffi Maxime Ayi Descartes <koffi.maxime@chem.loc>
2026-02-12 16:07:09 +0100

    navAbout 50%

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+
+  <div>
+    <nav>
+      <router-link to="/about">About</router-link>
+    </nav>
+    <main>
+    <router-view/>
+    </main>
+
+  </div>
+
 </template>
 
 <style scoped></style>
